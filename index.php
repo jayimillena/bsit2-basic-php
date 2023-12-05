a03e178bc1bdb6e86d2f3258cf1e7ea1ea8e26fa
Jay Millena <jay.millena07@gmail.com>
2023-12-05 11:04:59 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>IT Note</title>
+  <link href="style.css" type="css/text" rel="stylesheet" />
+</head>
+<body>
+  <div class="it-note-form">
+    <form action="index.php" method="post">
+      <div class="form-input">
+        <label for="note">Note</label>
+        <textarea name="note" id="" cols="30" rows="10" placeholder="Note here"></textarea>
+      </div>
+      <div class="form-input">
+        <input type="submit" name="save" value="Save">
+      </div>
+    </form>
+  </div>
+</body>
+</html>
